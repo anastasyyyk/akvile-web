8188d58e8aff5274b0c6b838be7049b024505c7a
anastasyyyk <82265279+anastasyyyk@users.noreply.github.com>
2021-08-27 17:49:03 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# akvile-webpage
---
+++ assets/script.js
@@ -0,0 +1,35 @@
+
+/*==================== show menu ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu');
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// ========= show scroll top ===========
+// function scrollTop(){
+//     const scrollTop = document.getElementById('scroll-top');
+//     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+//     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+// }
+// window.addEventListener('scroll', scrollTop);
